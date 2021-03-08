6a46f5b814a4952f2471dbfa1868be6fa01534a4
1210435219 <1210435219@qq.com>
2021-03-08 20:42:00 +0800

    三角形周长面积计算

---
+++ triangle S C/triangle S C/triangle.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float a , b , c ;
+	scanf("%f %f %f", &a, &b, &c);
+	float n = (a + b + c) / 2;
+	printf("circumference= %.2f area= %.2f ",a+b+c, sqrt(n * (n - a) * (n - b) * (n - c)));
+	return 0;
+}
