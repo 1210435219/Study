7f274500b3a9538b53f4ee68022a1b52d9cc1a5a
1210435219 <1210435219@qq.com>
2021-03-21 10:00:54 +0800

    test

---
+++ function test/function test/function_test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//int exchange(int* pa, int* pb)
+//{
+//	int temp;
+//	temp = *pa;
+//	*pa = *pb;
+//	*pb = temp;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("%d   %d\n", a, b);
+//	exchange(&a, &b);
+//	printf("%d   %d\n", a, b);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i;
+//	int j;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
