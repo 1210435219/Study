6db32763fb539cad313b641d6da44f81389d7e67
1210435219 <1210435219@qq.com>
2021-03-22 16:59:50 +0800

    function test

---
+++ function test02/function test02/function test02.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//void newline()
+//{
+//	printf("????\n");
+//}
+//
+//void three_line()
+//{
+//	int i;
+//	for (i = 0; i < 4; i++)
+//		newline();
+//}
+//
+//int main()
+//{
+//	three_line();
+//	return 0;
+//}
+
+//int main()
+//{
+//	//链式访问：strlen返回值作为printf的参数
+//	printf("%d\n", strlen("abcdef"));
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d",43)));
+//	//printf返回值为打印字符个数
+//	return 0;
+//}
+
+//int add(int x, int y);//使用前先声明，或 将函数写在主函数前
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("sum=%d\n", add(a, b));
+//	return 0;
+//}
+//
+//add(int x, int y)
+//{
+//	return x + y;
+//}
