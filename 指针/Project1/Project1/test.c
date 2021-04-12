35167b380e8fe0d7355a2d568aca9675a1ef1008
1210435219 <1210435219@qq.com>
2021-04-12 15:39:00 +0800

    test

---
+++ 指针/Project1/Project1/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int* p = NULL;
+//	if (p != NULL)//检验指针有效性
+//		*p = 20;
+//	return 0;
+//}
+
+//二级指针
+
+int main()
+{
+	int a = 10;
+	int* p = &a;//p是一级指针
+	int* *pp = &p;//pp是二级指针，定义方式为int** pp   存放p的地址
+	int** *ppp = &pp;//三级指针
+	**pp = 20;
+	printf("%d", a);
+	return 0;
+}
