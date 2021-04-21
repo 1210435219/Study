793648a9de6c2360853d1ebf638bfc93818e1b90
1210435219 <1210435219@qq.com>
2021-04-21 09:43:58 +0800

    1

---
+++ 指针逆序字符串/Project1/Project1/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+void Reverse(char* ps)
+{
+	char* left = ps;
+	char* right = ps + strlen(ps) - 1;
+	while (left < right)
+	{
+		char temp = *left;
+		*left = *right;
+		*right = temp;
+		left += 1;
+		right -= 1;
+	}
+}
+
+int main()
+{
+	//char str[100]="abc 123";
+	char str[100];
+	gets(str);
+	Reverse(str);
+	printf("%s", &str);
+
+	return 0;
+}
+
+
+//带空格输入字符串
+//1.用 gets() ，它可以接收带空格的字符串， 直到回车才结束输入
+//
+//char buf[80] = { 0 };
+//gets(buf);   //可以读取空格， 回车结束输入
+//
+//2.使用"%[^\n]" 也是可以接收输入一个带空格的字符串，直到回车结束接收
+//
+//char buf[10] = { 0 };
+//
+//scanf("%[^\n]", buf); //可以读取空格，回车结束输入
