44cdb2d50e56f661ade51f06e78ac9d221bb31c7
1210435219 <1210435219@qq.com>
2021-05-24 22:18:56 +0800

    2

---
+++ 字符函数/Project1/Project1/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//×Ö·û´®¿½±´strcpy
+int main()
+{
+	char arr1[20] = "wwwwwwwwwwwww";
+	char arr2[] = "HELLO";
+	strcpy(arr1, arr2);
+	printf("%s \n", arr1);
+	return 0;
+}
