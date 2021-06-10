a63456eaaee53bec19470760063c33d42f19fca4
1210435219 <1210435219@qq.com>
2021-06-10 22:17:45 +0800

    2

--- triangle S C/triangle S C/triangle.c
+++ triangle S C/triangle S C/triangle.c
@@ -7,6 +7,6 @@ int main()
 	float a , b , c ;
 	scanf("%f %f %f", &a, &b, &c);
 	float n = (a + b + c) / 2;
-	printf("circumference= %.2f area= %.2f ",a+b+c, sqrt(n * (n - a) * (n - b) * (n - c)));
+	printf("circumference= %.2f area= %.2f \n",a+b+c, sqrt(n * (n - a) * (n - b) * (n - c)));
 	return 0;
 }
